6ad778cb398a29c3e46b00d5b8e67cfb74932fe2
Deyvid <deyviimanol@gmail.com>
2025-09-03 10:04:57 -0500

    Primer commit

---
+++ src/main/java/com/primer_proyecto/primerproyecto/PrimerproyectoApplication.java
@@ -0,0 +1,16 @@
+package com.primer_proyecto.primerproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class PrimerproyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerproyectoApplication.class, args);
+	}
+
+}
+git add .
