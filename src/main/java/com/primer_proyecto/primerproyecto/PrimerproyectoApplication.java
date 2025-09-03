dd9ca7c94b2b05c44649858409dc4fa4bcc2dadd
Deyvid <deyviimanol@gmail.com>
2025-09-03 10:15:16 -0500

    Segundo Avance

--- src/main/java/com/primer_proyecto/primerproyecto/PrimerproyectoApplication.java
+++ src/main/java/com/primer_proyecto/primerproyecto/PrimerproyectoApplication.java
@@ -2,15 +2,22 @@ package com.primer_proyecto.primerproyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.SQLOutput;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PrimerproyectoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrimerproyectoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PrimerproyectoApplication.class, args);
+    }
 }
 
+@RestController
+class HolaController {
+
+    @GetMapping("/hola")
+    public String hola() {
+        return "¡Hola, Spring Boot funcionando!";
+    }
+}
